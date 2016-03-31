f4be890388734f2fc87f23bcd139f46d24709ce2
andyskvo <andyskv@gmail.com>
2016-03-31 09:06:23 +0300

    exceptions

--- src/main/java/ru/andyskvo/main/Main.java
+++ src/main/java/ru/andyskvo/main/Main.java
@@ -7,7 +7,6 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.LinkedList;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -15,50 +14,55 @@ import java.util.TreeMap;
  * Created by Skvorcov on 30.03.2016.
  */
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         String fileName = args[0].trim();
 
         Path path = Paths.get(fileName);
         Charset charset = Charset.forName("UTF-8");
         //BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
-        BufferedReader bufferedReader = Files.newBufferedReader(path, charset);
-        String lineContents;
-        String allStrings = "";
-        while ((lineContents = bufferedReader.readLine()) != null) {
-            allStrings = allStrings + lineContents;
+        try(BufferedReader bufferedReader = Files.newBufferedReader(path, charset)) {
+            String lineContents;
+            String allStrings = "";
+            while ((lineContents = bufferedReader.readLine()) != null) {
+                allStrings = allStrings + lineContents;
+            }
+
+            int openTagIndex = allStrings.indexOf('[') + 1;
+            String openTag = allStrings.substring(0, openTagIndex);
+
+            if (openTag.contains("phpreturn")) {
+                openTag = openTag.replace("php", "php\n");
+            }
+
+            int closeTagIndex = allStrings.indexOf(']');
+            String closeTag = allStrings.substring(closeTagIndex);
+
+            String dict = allStrings.substring(openTagIndex, closeTagIndex).trim();
+
+            String[]allPairs = dict.split(",");
+
+            TreeMap<String, String> map = new TreeMap<>();
+
+            for (String pair : allPairs) {
+                String[] arr = pair.split("=>");
+                String key = arr[0].replace("'", "").trim();
+                String val = arr[1].replaceAll("'", "").trim();
+                map.put(key, val);
+            }
+            //PrintWriter printWriter = new PrintWriter(new FileWriter(fileName));
+            try(PrintWriter printWriter = new PrintWriter(Files.newBufferedWriter(path))) {
+                printWriter.println(openTag);
+                for (Map.Entry<String, String> p : map.entrySet()) {
+                    printWriter.println("    '" + p.getKey() + "' => '" + p.getValue() + "',");
+                }
+                printWriter.println(closeTag);
+
+            } catch (SecurityException e) {
+                System.out.println("Write access error");
+            }
+
+        } catch (IOException e) {
+            System.out.println("File " + path + " not found");
         }
-
-        int openTagIndex = allStrings.indexOf('[') + 1;
-        String openTag = allStrings.substring(0, openTagIndex);
-
-        if (openTag.contains("phpreturn")) {
-            openTag = openTag.replace("php", "php\n");
-        }
-
-        int closeTagIndex = allStrings.indexOf(']');
-        String closeTag = allStrings.substring(closeTagIndex);
-
-        String dict = allStrings.substring(openTagIndex, closeTagIndex).trim();
-
-        String[]allPairs = dict.split(",");
-
-        TreeMap<String, String> map = new TreeMap<>();
-
-        for (String pair : allPairs) {
-            String[] arr = pair.split("=>");
-            String key = arr[0].replace("'", "").trim();
-            String val = arr[1].replaceAll("'", "").trim();
-            map.put(key, val);
-        }
-
-        //PrintWriter printWriter = new PrintWriter(new FileWriter(fileName));
-        PrintWriter printWriter = new PrintWriter(Files.newBufferedWriter(path));
-        printWriter.println(openTag);
-        for (Map.Entry<String, String> p : map.entrySet()) {
-            printWriter.println("    '" + p.getKey() + "' => '" + p.getValue() + "',");
-        }
-        printWriter.println(closeTag);
-
-        printWriter.close();
     }
 }
